1b097d5204926eab4dafad7d29d1b5e14652e097
kartik saxena <kartiksaxena@kartiks-MacBook-Pro.local>
2023-05-09 16:19:49 -0400

    build: routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import CreateBooking from './components/booking/create_booking';
 import AllBooking from './components/booking/all_bookings';
+import Layout from './components/helper/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <CreateBooking></CreateBooking>
-      {/* <AllBooking></AllBooking> */}
-    </div>
+    // <div className="App">
+    //   <CreateBooking></CreateBooking>
+    //   {/* <AllBooking></AllBooking> */}
+    // </div>
+    <BrowserRouter>
+      <Routes path="/" element={<Layout />}>
+          <Route path="create_booking" element={<CreateBooking />} />
+          <Route path="all_bookings" element={<AllBooking />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
