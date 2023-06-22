4b93cc893d8d6c08cfe351561e836cbb3fc34bbf
KARTIK SAXENA <46376692+kartiks2000@users.noreply.github.com>
2023-06-22 14:37:50 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import {getDatabase} from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzxRXTJ5YsCkodbMdELVu1FV4yXUsa5VU",
-  authDomain: "casaamerico-reservation.firebaseapp.com",
-  projectId: "casaamerico-reservation",
-  storageBucket: "casaamerico-reservation.appspot.com",
-  messagingSenderId: "456388569495",
-  appId: "1:456388569495:web:3c7f3f425b875628817cd2",
-  measurementId: "G-W3QHZ38ZMB"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
